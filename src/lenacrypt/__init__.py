fef499f9329aa3c5abb4624ee2370a6257d4fada
Lena Merkli <lena@merkli.me>
2025-01-25 19:34:59 +0100

    Initialize pypi project

---
+++ src/lenacrypt/__init__.py
@@ -0,0 +1,14 @@
+
+
+__all__ = [
+    'VERSION',
+    'LICENSE',
+    'AUTHORS',
+    'COPYRIGHT'
+]
+
+
+VERSION = '0.0.1'
+LICENSE = 'MIT'
+AUTHORS = 'Lena Merkli'
+COPYRIGHT = 'Copyright (c) 2023 Lena Merkli'
